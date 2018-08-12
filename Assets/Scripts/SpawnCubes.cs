96a64d4636df3a02e941a523199e3d017d74dba6
H. K. T. Poggie <henry.poggie@gmail.com>
2018-08-12 17:13:34 -0400

    Increase spawn rate

--- Assets/Scripts/SpawnCubes.cs
+++ Assets/Scripts/SpawnCubes.cs
@@ -4,9 +4,21 @@ public class SpawnCubes : MonoBehaviour {
 	public GameObject obj;
 	public float SpawnDelay;
 	public float SpawnInterval;
+    public float SpawnIntervalDecreaseRate;
 
-	void Start () {
-		InvokeRepeating("Spawn", SpawnDelay, SpawnInterval);
+    private bool hasSpawned;
+    private float timer;
+
+	void Update () {
+		timer += Time.deltaTime;
+        if ((hasSpawned && timer > SpawnInterval) 
+            || (!hasSpawned && timer > SpawnDelay))
+        {
+            timer = 0;
+            Spawn();
+            SpawnInterval *= SpawnIntervalDecreaseRate;
+            hasSpawned = true;
+        }
 	}
 
 	void Spawn () {
