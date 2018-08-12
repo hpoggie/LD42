ddaf2531b3153b7eca9528ff90e0ccc744a80485
H. K. T. Poggie <henry.poggie@gmail.com>
2018-08-12 17:54:33 -0400

    Add time survived

---
+++ Assets/TimeSurvived.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeSurvived : MonoBehaviour {
+    void Update () {
+        // https://answers.unity.com/questions/326311/formatting-a-tostring-for-time.html
+        var timeSeconds = (int)Time.time;
+		GetComponent<Text>().text = string.Format("You survived {0:00}:{1:00}", timeSeconds / 60, timeSeconds % 60);
+	}
+}
