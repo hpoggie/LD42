e24df0d3fa8dfc7d660f149426fb043787e10362
H. K. T. Poggie <henry.poggie@gmail.com>
2018-08-12 16:52:05 -0400

    Add menu

---
+++ Assets/Loader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour {
+
+	void Start () {
+		GetComponent<Button>().onClick.AddListener(Load);
+	}
+	
+	void Load () {
+		SceneManager.LoadScene(1);
+	}
+}
